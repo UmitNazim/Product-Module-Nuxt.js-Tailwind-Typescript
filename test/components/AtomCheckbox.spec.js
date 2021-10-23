8a25b1b2a9dd882ce3f7e4c25a6564e3b4134cf9
Ümit nazım Çevik <cevikumitnazim@gmail.com>
2021-10-23 11:48:50 +0300

    atom-checkbox components unit tests have been completed.

---
+++ test/components/AtomCheckbox.spec.js
@@ -0,0 +1,50 @@
+import { shallowMount } from '@vue/test-utils';
+import AtomCheckbox from '../../src/components/AtomCheckbox';
+
+describe('AtomCheckbox', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(AtomCheckbox);
+  });
+
+  describe('General', () => {
+    it('checks whether componenet was mounted or not', () => {
+      expect(wrapper.exists()).toBeTruthy();
+    });
+  });
+
+  describe('Props', () => {
+    describe('value', () => {
+      it('checks prop acceptable value type', () => {
+        expect(wrapper.vm.$options.props.value).toEqual({ type: Boolean, default: false });
+      });
+    });
+    describe('label', () => {
+      it('checks prop acceptable value type', () => {
+        expect(wrapper.vm.$options.props.label).toEqual({ type: String, default: null });
+      });
+    });
+  });
+
+  describe('Data', () => {
+    describe('proxy', () => {
+      it('checks data default value', () => {
+        expect(wrapper.vm.proxy).toBeFalsy();
+      });
+    });
+  });
+
+  describe('Html', () => {
+    describe('input', () => {
+      it('checks html elements id attribute.', async () => {
+        wrapper.setProps({ label: 'waste-text' });
+        await wrapper.vm.$nextTick();
+        expect(wrapper.find('.atom-checkbox__input').attributes('id')).toEqual('waste-text');
+      });
+      it('checks html elements change event.', async () => {
+        await wrapper.find('.atom-checkbox__input').setChecked();
+        expect(wrapper.emitted('input')).toBeTruthy();
+      });
+    });
+  });
+});
